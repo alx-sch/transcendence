2341a39d709b6fd4267a01d35261cf4556afc133
Johannes Dach <johannesdach@gmail.com>
2025-11-24 14:40:03 +0100

    just saving messy development

---
+++ src/frontend/eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.js
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json", // IMPORTANT: this enables type-aware linting
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+];
